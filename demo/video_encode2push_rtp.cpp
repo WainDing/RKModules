6a029ce36193b166d9160dbf55d759d430a75f08
tiger <tiger@ubuntu>
2018-05-27 23:22:24 +0800

    [demo]: modify rtp demo
    Signed-off-by: tiger <tiger@ubuntu>

--- demo/video_encode2push_rtp.cpp
+++ demo/video_encode2push_rtp.cpp
@@ -1,13 +1,33 @@
 #include <stdio.h>
+#include "cam.h"
+#include "codec.h"
+#include "stream_media.h"
+#include "log.h"
 
-#define log(fmt,...)\
-    do {\
-        printf(fmt,##__VA_ARGS__);\
-        printf("\n");\
-    } while (0)
+#define TAG "video_encode_test"
+#define RET_VAL(ret,fun)\
+    if (ret) {\
+        log(TAG, "%s failed", fun);\
+        return -1;\
+    } else {\
+        log(TAG, "%s success", fun);\
+    }
+
+#define W 1280
+#define H 720
 
 int main(int argc, char **argv) {
-    log("video_encode2push_rtp");
+    int ret = 0;
+    RKMediaCam cam;
+    RKMediaCodec codec;
+    RKMediaStreamContext media;
 
+    log(TAG, "video_encode2push_rtp");
+
+    ret = cam.Init("/dev/video0", V4L2_PIX_FMT_YUYV, W, H);
+    RET_VAL(ret, "cam.Init");
+
+    ret = cam.Deinit();
+    RET_VAL(ret, "cam.Deinit");
     return 0;
 }
